bb09a947e6cab085e728883e2414669195c769b1
MythicalPlayz <eyaddodo05@gmail.com>
2023-12-27 02:11:23 +0200

    removed non GUI

--- Pyramid_Player.h
+++
@@ -1,8 +0,0 @@
-#include "BoardGame_Classes.hpp"
-
-class pyramid_Player : public Player {
-public:
-    pyramid_Player (char symbol);
-    pyramid_Player (int order, char symbol);
-    void get_move(int &x, int &y);
-};
--- TicTacToePlayer.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "TicTacToePlayer.h"
-
-TicTacToePlayer::TicTacToePlayer(int o, char symbol) : Player(o, symbol){}
-
-void TicTacToePlayer::initialize_move(int &a, int &b) {
-    cout << "\n Please enter your move's coordinates (a and b): ";
-    cin >> a >> b;
-}
