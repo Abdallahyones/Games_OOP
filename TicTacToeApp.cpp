0faa168f56695f3e057faf576763a4090f624a33
Seif <112402478+Seif8886@users.noreply.github.com>
2024-01-15 18:44:26 +0200

    System changing, making the opponent plays with O

--- TicTacToeApp.cpp
+++ TicTacToeApp.cpp
@@ -8,12 +8,12 @@ int main(){
     Player* opponents[2];
     int choice;
     cout << "Welcome to X-O TicTacToe 5x5 Game. \n";
-    opponents[0] = new TicTacToePlayer(1, 'X');
+    opponents[0] = new TicTacToePlayer(1, 'O');
     cout << "Press 1 if you want to play with computer: ";
     cin >> choice;
     switch(choice){
         case 1:
-            opponents[1] = new RandomPlayer('O',5,5);
+            opponents[1] = new RandomPlayer('X',5,5);
             break;
         case 2:
             opponents[1] = new TicTacToePlayer(2, 'O');
